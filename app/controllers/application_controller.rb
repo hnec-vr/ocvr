689751f566d8f1daa7fbb2dce17787aafd604a62
Abi Noda <a@abinoda.com>
2013-10-12 20:40:46 -0500

    sets application locale via session

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,17 @@
 class ApplicationController < ActionController::Base
+  include SimpleCaptcha::ControllerHelpers
+
   protect_from_forgery
 
-  before_filter :no_layout_if_xhr
+  before_filter :no_layout_if_xhr, :set_locale
 
   private
 
   def no_layout_if_xhr
     false if request.xhr?
+  end
+
+  def set_locale
+    I18n.locale = session[:locale] || I18n.default_locale
   end
 end
