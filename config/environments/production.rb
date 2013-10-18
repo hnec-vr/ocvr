0eeb5a6d06ddf168f1f203bea547b2c297d74624
Abi Noda <a@abinoda.com>
2013-10-17 22:58:32 -0500

    adds sendgrid configuration to production environment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -45,4 +45,16 @@ OCVR::Application.configure do
 
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
+
+  # Configuration for using SendGrid on Heroku
+  ActionMailer::Base.delivery_method = :smtp
+  ActionMailer::Base.smtp_settings = {
+    :user_name => ENV["SENDGRID_USERNAME"],
+    :password => ENV["SENDGRID_PASSWORD"],
+    :domain => "ocvr-production.herokuapp.com",
+    :address => "smtp.sendgrid.net",
+    :port => 587,
+    :authentication => :plain,
+    :enable_starttls_auto => true
+  }
 end
