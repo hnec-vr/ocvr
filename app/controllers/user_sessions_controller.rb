dddcd9cbf88d614816e86e342229e17a13e20e0e
Abi Noda <a@abinoda.com>
2013-10-27 18:08:06 -0500

    prevents nil user session exceptions on logout

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -15,7 +15,7 @@ class UserSessionsController < ApplicationController
   end
 
   def destroy
-    current_user_session.destroy
+    current_user_session.try(:destroy)
 
     redirect_to root_path
   end
