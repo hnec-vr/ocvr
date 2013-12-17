14eb8abe82d3674a0a17d71f861b3afb49efc2b4
Abi Noda <a@abinoda.com>
2013-12-17 08:19:19 +0700

    sets admin locale to en

--- app/controllers/admin/base_controller.rb
+++ app/controllers/admin/base_controller.rb
@@ -1,5 +1,10 @@
 class Admin::BaseController < ApplicationController
   http_basic_authenticate_with name: ENV["ADMIN_USERNAME"], password: ENV["ADMIN_PASSWORD"]
-
   layout "admin"
+
+  before_filter :set_locale_to_en
+
+  def set_locale_to_en
+    I18n.locale = :en
+  end
 end
